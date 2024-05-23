f09a78cfa5b6da56c1eb10efcae72cc22e750781
Arshid Ahmad <arshid.ahmad1@gmail.com>
2024-05-23 15:29:24 +0530

    My first Commit

--- collecting details/Program.cs
+++ collecting details/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace UserProfile
+{
+    class EmpInfo
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Data Collection Form");
+
+            // Collecting name
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            // Collecting email
+            Console.Write("Enter your email address: ");
+            string email = Console.ReadLine();
+
+            // Collecting phone number
+            Console.Write("Enter your Phone Number: ");
+            long phoneNumber = Convert.ToInt64(Console.ReadLine());
+
+            // Displaying user profile
+            Console.WriteLine("\nUser Profile:");
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Email: " + email);
+            Console.WriteLine("Phone Number: " + phoneNumber);
+
+            Console.ReadLine();
+
+        }
+    }
+}
